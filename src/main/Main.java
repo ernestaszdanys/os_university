f2d70f4c79a55d469d498f177dd4286435bbf978
ernestaszdanys <ernestas13@gmail.com>
2015-03-17 14:20:07 +0200

    Copied Word class with thousands of bugs...

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("laba diena");
+
+        Word w1 = new Word(Word.intToWord(128));
+        System.out.println(w1);
+
     }
 
 }
--- src/main/Word.java
+++ src/main/Word.java
@@ -1,10 +1,68 @@
 package main;
 
+import javax.swing.plaf.SeparatorUI;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
 /**
  * Created by Ernestas on 2015-03-17.
  */
 public class Word {
     public static final int SIZE = 4;
+    private byte[] data; // FIXME: make data final?
 
+    public Word() {
+        this.data = new byte[SIZE];
+    }
 
+    public Word(Word word) {
+        this.data = word.data.clone();
+    }
+
+    public byte getByte(int index) {
+        return data[index];
+    }
+
+    // TODO: setByte, but should data be final?..
+    public void setByte(int index, byte info) {
+        data[index] = info;
+    }
+
+    @Override
+    protected Word clone() {  // FIXME: This method should throw: throws CloneNotSupportedException
+        return new Word(this);
+    }
+
+    public boolean equals(Word word) {
+        return Arrays.equals(data, word.data);
+    }
+
+    public static int wordToInt(Word word) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(SIZE);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.clear();
+        for (int i = 0; i < SIZE; i++) {
+            bb.put(word.getByte(i));
+        }
+        bb.position(0);
+        return bb.getInt();
+    }
+
+    public static Word intToWord(int value) {
+        ByteBuffer bb = ByteBuffer.allocate(SIZE);
+        bb.order(ByteOrder.LITTLE_ENDIAN);
+        bb.clear();
+        bb.putInt(value);
+        Word word = new Word();
+        for (int i = 0; i < SIZE; i++) {
+            word.setByte(i, bb.get(i));  // FIXME: data final????
+        }
+        return word;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(data);
+    }
 }
