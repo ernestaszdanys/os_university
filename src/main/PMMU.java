42a94435e87c09d963ca464e434e8446ae035df8
Kompiuteris <Kompiuteris@Kompiuteriss-MacBook-Air.local>
2015-03-18 11:12:10 +0200

    PPMU

--- src/main/PMMU.java
+++ src/main/PMMU.java
@@ -1,7 +1,19 @@
 package main;
 
 /**
- * Created by Marius on 2015-03-17.
+ * Created by Rokas on 2015-03-17.
  */
 public class PMMU {
+    public final static int BLOCKSIZE = 16;
+    private final int userBlocks;
+    private final int supervisorBlocks;
+    private CPU cpu;
+    private RealMemory realMemory;
+    private RealMachine realMachine;
+
+    public PMMU(CPU cpu, Memory realMemory, RM realMachine){
+        this.cpu = cpu;
+        this.realMemory = realMemory;
+        this.realMachine = realMachine;
+    }
 }
