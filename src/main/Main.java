89903262f74ca83574c70b8fb0808fad19e55fcd
ernestaszdanys <ernestas13@gmail.com>
2015-03-20 12:04:24 +0200

    Added InputDevice class for reading data from file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,29 @@
 package main;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args){
-        CPU cpu = new CPU();
-        VirtualMachine VM1 = new VirtualMachine();
-        PMMU.setVirtualMachine(VM1);
-        cpu.cmdPUNx(10);
-        cpu.cmdPUNx(45);
-        cpu.cmdADD();
-        VM1.printMemory();
+    public static void main(String[] args) {
 
+        try {
+            InputDevice.openFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Wrong file name.");
+        }
+
+        Word[] words;
+        try {
+            while (true) {
+                words = InputDevice.getInput();
+                for (Word w : words) {
+                    System.out.println(w);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Reading finished.");
+        }
     }
 
 }
