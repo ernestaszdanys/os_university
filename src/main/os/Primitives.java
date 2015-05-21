299f8ba1f7939cd79c8188bf08238b0420d2f31c
ernestaszdanys <ernestas13@gmail.com>
2015-05-21 14:49:09 +0300

    masterclass changes

--- src/main/os/Primitives.java
+++ src/main/os/Primitives.java
@@ -4,13 +4,13 @@ package main.os;
  * Created by Marius on 2015-05-21.
  */
 public class Primitives {
-    public static void NAIKINTIIP(){}
-    public static void STABDYTIIP(){}
-    public static void AKTYVUOTIP(){}
-    public static void KURTIP(){}
-    public static void KEISTIPP(){}
-    public static void KURTIR(){}
-    public static void NAIKINTIR(){}
-    public static void PRASYTIR(){}
-    public static void ATLAISVINTIR(){}
+    public static void deleteProcess(){}
+    public static void stopProcess(){}
+    public static void activateProcess(){}
+    public static void createProcess(){}
+    public static void changeProcessPriority(){}
+    public static void createResource(){}
+    public static void deleteResource(){}
+    public static void requestResource(){}
+    public static void freeResource(){}
 }
