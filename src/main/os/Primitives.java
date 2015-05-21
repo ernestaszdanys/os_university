a8763badf4375bb651ac94891afb7981f1a770a7
ernestaszdanys <ernestas13@gmail.com>
2015-05-21 14:35:40 +0300

    2

---
+++ src/main/os/Primitives.java
@@ -0,0 +1,16 @@
+package main.os;
+
+/**
+ * Created by Marius on 2015-05-21.
+ */
+public class Primitives {
+    public static void NAIKINTIIP(){}
+    public static void STABDYTIIP(){}
+    public static void AKTYVUOTIP(){}
+    public static void KURTIP(){}
+    public static void KEISTIPP(){}
+    public static void KURTIR(){}
+    public static void NAIKINTIR(){}
+    public static void PRASYTIR(){}
+    public static void ATLAISVINTIR(){}
+}
---
+++ src/main/os/ResourceDivider.java
@@ -0,0 +1,7 @@
+package main.os;
+
+/**
+ * Created by Marius on 2015-05-21.
+ */
+public class ResourceDivider {
+}
