eeb720c7a3dcc3df56c32f5bc8f312130253723b
Mariius <zilinskas.m@gmail.com>
2015-05-21 13:53:21 +0300

    Resource Descriptor

--- src/main/os/ResourceDescriptor.java
+++ src/main/os/ResourceDescriptor.java
@@ -1,7 +1,17 @@
 package main.os;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Kompiuteris on 15-05-21.
  */
 public class ResourceDescriptor {
+    public static List<Integer> Rid = new ArrayList<Integer>();
+    public static List<Boolean> PNR = new ArrayList<Boolean>();
+    public static List<Integer> K = new ArrayList<Integer>();
+    // TODO: kas čia???
+    public static List<Integer> PA = new ArrayList<Integer>();
+    public static List<Resource> LPS = new ArrayList<Resource>();
+    public static List<ResourceDivider> PASK = new ArrayList<ResourceDivider>();
 }
